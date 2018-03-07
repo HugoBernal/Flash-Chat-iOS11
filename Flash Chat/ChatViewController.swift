d37390523c1ee1a9749ab796250d1023e8529a6c
Hugo Bernal <hugonobernal@gmail.com>
2018-03-06 23:10:10 -0500

    Declared the properties the message (model) is going to have. Implemented conformance to the UITextFieldDelegate and animation for the view that contains the messageTextField so it animatedly changes its height when the keyboard is shown, and also to return to its original height if any part of the tableView is tapped.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
     // Declare instance variables here
 
@@ -32,11 +32,12 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         //TODO: Set yourself as the delegate of the text field here:
 
-        
+        messageTextfield.delegate = self
         
         //TODO: Set the tapGesture here:
         
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
 
         //TODO: Register your MessageCell.xib file here:
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
@@ -50,11 +51,13 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     //MARK: - TableView DataSource Methods
     
+    //TODO: Declare cellForRowAtIndexPath here:
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First Message", "Second Messageñdhjsfajshdfkjashdfkjahsdlfkashdlkfjhaskdfjhaslkdjfhalksjdhfaklsjdhflaksjdhfaklsjdhfaksljdhfalksdhjfalksdhfaklsdhflaskdhjfalksjdhflaksdjhfalksdjhlakjhfjkasdhfajkslhdfajkshdfjklasdhflakshdfjhasdjkfhlalkshfjfhajsdbcnazcbhjdbcalsdjbcqprbjcaksjhdgfasybecaoiusfhbcalsjkdhlbavkjdhfbaovsiufhabcilushiacubhsdlcashjdks", "Third Message"]
+        let messageArray = ["First Message", "Second Message", "Third Message"]
         
         cell.messageBody.text = messageArray[indexPath.row]
         
@@ -62,19 +65,17 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
     }
     
-    //TODO: Declare cellForRowAtIndexPath here:
+    //TODO: Declare numberOfRowsInSection here:
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
-    //TODO: Declare numberOfRowsInSection here:
-    
-    
-    
     //TODO: Declare tableViewTapped here:
     
-    
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
     
     //TODO: Declare configureTableView here:
     
@@ -87,16 +88,27 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     //MARK:- TextField Delegate Methods
     
-    
-
-    
     //TODO: Declare textFieldDidBeginEditing here:
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
     
-    
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
+
     
     //TODO: Declare textFieldDidEndEditing here:
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+        
+    }
 
     
     ///////////////////////////////////////////
